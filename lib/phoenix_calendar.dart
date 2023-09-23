7e6f4677927bbc189ee117b0788cd04d8d0a8038
lipeng <1162423147@qq.com>
2023-09-23 18:49:09 +0800

    [new] 新建

---
+++ lib/phoenix_calendar.dart
@@ -0,0 +1,12 @@
+/*
+ * @Author: lipeng 1162423147@qq.com
+ * @Date: 2023-09-23 18:37:25
+ * @LastEditors: lipeng 1162423147@qq.com
+ * @LastEditTime: 2023-09-23 18:47:18
+ * @FilePath: /phoenix_calendar/lib/phoenix_calendar.dart
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+library phoenix_calendar;
+
+//
+export 'calendar_view.dart';
